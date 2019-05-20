0e46c60e77dd0fab75ffd61ccdea514dedd6642e
Anwar Abdullah <anwarabdullahn28@gmail.com>
2019-05-20 22:07:21 +0700

    user login

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,7 +7,7 @@ const User = require('../models/User'),
 module.exports.Register = (req, res) => {
 	const { first_name, last_name, email, password, phone } = req.body,
 		newUser = new User({ first_name, last_name, email, phone }),
-		{ errors, isValid } = Validation.Register(req.body);
+		{ errors, isValid } = UserValidation.Register(req.body);
 
 	!isValid
 		? res.status(400).json({
@@ -36,5 +36,40 @@ module.exports.Register = (req, res) => {
 									});
 								})
 				)
+				.catch((err) => res.status(400).json(err));
+};
+
+module.exports.Login = (req, res) => {
+	const { email, password } = req.body,
+		{ errors, isValid } = UserValidation.Login(req.body);
+
+	!isValid
+		? res.status(400).json({
+				success: false,
+				msg: `Error Validation`,
+				errors
+			})
+		: User.findOne({ email })
+				.then((user) => {
+					!user
+						? res.status(404).json({
+								success: false,
+								msg: `User not found`
+							})
+						: bcrypt.compare(password, user.password).then((match) => {
+								const payload = { id: user.id, email, role: 'user' };
+								!match
+									? res.status(400).json({
+											success: false,
+											msg: `Inccorect Password`
+										})
+									: jwt.sign(payload, secret, (err, token) =>
+											res.json({
+												success: true,
+												token: 'Bearer ' + token
+											})
+										);
+							});
+				})
 				.catch((err) => res.status(400).json(err));
 };
--- validations/UserValidation.js
+++ validations/UserValidation.js
@@ -20,3 +20,16 @@ module.exports.Register = (data) => {
 		isValid: isEmpty(errors)
 	};
 };
+
+module.exports.Login = (data) => {
+	let errors = {};
+	data.email = !isEmpty(data.email) ? data.email : '';
+	data.password = !isEmpty(data.password) ? data.password : '';
+
+	if (Validator.isEmpty(data.email)) errors.email = `Email is required`;
+	if (Validator.isEmpty(data.password)) errors.password = `Password is required`;
+	return {
+		errors,
+		isValid: isEmpty(errors)
+	};
+};
